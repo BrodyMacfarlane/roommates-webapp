1ccaedd64c9d1f295ac4d3c6935ea93eae87f6e1
Brody Macfarlane <macfarlanebrody@gmail.com>
2023-10-26 16:08:09 -0600

    fix dir

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   apps: [{
-    script: '/root/roommates/current/backend/build/server.js',
-    watch: ['/root/roommates/current/backend'],
-    ignore_watch: ['/root/roommates/current/backend/node_modules']
+    script: '/root/roommates-webapp/current/backend/build/server.js',
+    watch: ['/root/roommates-webapp/current/backend'],
+    ignore_watch: ['/root/roommates-webapp/current/backend/node_modules']
   }],
   deploy: {
     production: {
@@ -10,9 +10,9 @@ module.exports = {
       host: '159.223.192.156',
       ref: 'origin/main',
       repo: 'git@github.com:BrodyMacfarlane/roommates-webapp.git',
-      path: '/root/roommates',
+      path: '/root/roommates-webapp',
       'pre-deploy-local': '',
-      'post-deploy': 'cd /root/roommates/current/backend && npm install && npm run build && cd build && cp /root/roommates/backend/.env .env && pm2 stop all && pm2 delete all && pm2 start /root/roommates/current/ecosystem.config.js',
+      'post-deploy': 'cd /root/roommates-webapp/current/backend && npm install && npm run build && cd build && cp /root/roommates-webapp/backend/.env .env && pm2 stop all && pm2 delete all && pm2 start /root/roommates-webapp/current/ecosystem.config.js',
       'pre-setup': ''
     }
   }
