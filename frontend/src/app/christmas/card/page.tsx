2c71ba49a77e2b74f1ee2d319dd85bdb8c3a33c1
Brody Macfarlane <macfarlanebrody@gmail.com>
2024-03-08 20:07:21 -0700

    qol

---
+++ frontend/src/app/christmas/card/page.tsx
@@ -0,0 +1,20 @@
+import ChristmasCardClientPage from '@/app/client-pages/ChristmasCard'
+import localFont from '@next/font/local'
+
+const handwriting = localFont({
+  src: [
+    {
+      path: '../../../assets/christmas/fonts/handwriting.ttf',
+      weight: '400',
+    },
+  ],
+  variable: '--font-handwriting',
+})
+
+export default function ChristmasCard() {
+  return (
+    <main className={`${handwriting.variable} font-handwriting uppercase text-2xl md:text-3xl`}>
+      <ChristmasCardClientPage />
+    </main>
+  )
+}
