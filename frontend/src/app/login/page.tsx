35ee7981200cebce5351678c79c4e5cbeb84dd07
Brody Macfarlane <macfarlanebrody@gmail.com>
2024-06-09 14:55:18 -0600

    fix suspense errors

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -3,13 +3,21 @@ import LargeForm from '@/components/content/LargeForm'
 import LoginForm from '@/components/forms/LoginForm'
 import Image from 'next/image'
 import Quiggo from '@/assets/img/quiggo.jpg'
+import { Suspense } from 'react'
 
 export default function Login() {
   return (
     <main className="pt-0">
       <HeroBackground hideMobile>
         <div className="pt-24 pb-12">
-          <LargeForm form={<LoginForm />} image={Quiggo} />
+          <LargeForm
+            form={
+              <Suspense>
+                <LoginForm />
+              </Suspense>
+            }
+            image={Quiggo}
+          />
         </div>
       </HeroBackground>
     </main>
--- frontend/src/app/signup/page.tsx
+++ frontend/src/app/signup/page.tsx
@@ -2,13 +2,21 @@ import HeroBackground from '@/components/content/HeroBackground'
 import LargeForm from '@/components/content/LargeForm'
 import SignupForm from '@/components/forms/SignupForm'
 import Quiggo from '@/assets/img/quiggo.jpg'
+import { Suspense } from 'react'
 
 export default function Signup() {
   return (
     <main className="pt-0">
       <HeroBackground hideMobile>
         <div className="pt-24 pb-12">
-          <LargeForm form={<SignupForm />} image={Quiggo} />
+          <LargeForm
+            form={
+              <Suspense>
+                <SignupForm />
+              </Suspense>
+            }
+            image={Quiggo}
+          />
         </div>
       </HeroBackground>
     </main>
